801695b68273d4043cfed7ea477e63b0b43bc6e3
Omar Mashaal <omarwmashaal@gmail.com>
2023-12-09 18:00:05 +0200

    Candidate details

---
+++ cariro_implant_academy/lib/features/user/data/candidateDetailsModel.dart
@@ -0,0 +1,56 @@
+import 'dart:convert';
+
+import 'package:cariro_implant_academy/core/domain/entities/BasicNameIdObjectEntity.dart';
+import 'package:cariro_implant_academy/core/features/settings/data/models/ImplantModel.dart';
+import 'package:cariro_implant_academy/core/features/settings/domain/entities/implantEntity.dart';
+import 'package:equatable/equatable.dart';
+
+import '../../../Models/CandidateDetails.dart';
+import '../../../Models/ImplantModel.dart';
+import '../../../core/data/models/BasicNameIdObjectModel.dart';
+import '../domain/entities/canidateDetailsEntity.dart';
+
+
+class CandidateDetailsModel extends CandidateDetailsEntity {
+  CandidateDetailsModel({
+    int? patientId,
+    BasicNameIdObjectModel? patient,
+    String? procedure,
+    DateTime? date,
+    int? implantCount,
+    List<String>? otherProcedures,
+    int? totalImplantCounts,
+    int? tooth,
+    int? implantId,
+    ImplantEntity? implant,
+  }) : super(
+    patientId: patientId,
+    patient: patient,
+    procedure: procedure,
+    date: date,
+    implantCount: implantCount,
+    otherProcedures: otherProcedures,
+    totalImplantCounts: totalImplantCounts,
+    tooth: tooth,
+    implantId: implantId,
+    implant: implant,
+  );
+
+  factory CandidateDetailsModel.fromJson(String source) =>
+      CandidateDetailsModel.fromMap(json.decode(source));
+
+  factory CandidateDetailsModel.fromMap(Map<String, dynamic> map) {
+    return CandidateDetailsModel(
+      patientId: map['patientId'],
+      patient:map['patient']==null?null: BasicNameIdObjectModel.fromJson(map['patient']),
+      procedure: map['procedure'],
+      date: DateTime.tryParse(map['date']),
+      implantCount: map['implantCount'],
+      otherProcedures:(map['otherProcedures'])?.map((e)=>e as String).toList(),
+      totalImplantCounts: map['totalImplantCounts'],
+      tooth: map['tooth'],
+      implantId: map['implantId'],
+      implant:map['patient']==null?null: ImplantModel.fromJson(map['implant']),
+    );
+  }
+}
