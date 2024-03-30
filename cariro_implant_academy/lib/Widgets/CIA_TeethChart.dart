b339dc5c127a5430c7d726328d1cad88b1c69cfd
Omar Mashaal <omarwmashaal@gmail.com>
2024-03-30 21:46:41 +0200

    Select Upper and lower teeth

--- cariro_implant_academy/lib/Widgets/CIA_TeethChart.dart
+++ cariro_implant_academy/lib/Widgets/CIA_TeethChart.dart
@@ -25,163 +25,264 @@ class _CIA_TeethChartState extends State<CIA_TeethChart> {
   @override
   Widget build(BuildContext context) {
     if (widget.selectedTeeth == null) widget.selectedTeeth = [];
-    return Row(
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
       children: [
-        Expanded(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Row(
-                children: [
-                  Expanded(
-                    child: CIA_MultiSelectChipWidget(
-                      key: GlobalKey(),
-                      onChange: (item, isSelected) {
-                        if (isSelected)
-                          selectedTeeth.add(int.parse(item));
-                        else
-                          selectedTeeth.remove(int.parse(item));
-                        if (widget.onChange != null) widget.onChange!(selectedTeeth);
-                      },
-                      labels: [
-                        CIA_MultiSelectChipWidgeModel(label: "18", isSelected: widget.selectedTeeth!.contains(18)),
-                        CIA_MultiSelectChipWidgeModel(label: "17", isSelected: widget.selectedTeeth!.contains(17)),
-                        CIA_MultiSelectChipWidgeModel(label: "16", isSelected: widget.selectedTeeth!.contains(16)),
-                        CIA_MultiSelectChipWidgeModel(label: "15", isSelected: widget.selectedTeeth!.contains(15)),
-                        CIA_MultiSelectChipWidgeModel(label: "14", isSelected: widget.selectedTeeth!.contains(14)),
-                        CIA_MultiSelectChipWidgeModel(label: "13", isSelected: widget.selectedTeeth!.contains(13)),
-                        CIA_MultiSelectChipWidgeModel(label: "12", isSelected: widget.selectedTeeth!.contains(12)),
-                        CIA_MultiSelectChipWidgeModel(label: "11", isSelected: widget.selectedTeeth!.contains(11)),
-                      ],
-                    ),
-                  ),
-                  Expanded(
-                    child: CIA_MultiSelectChipWidget(
-                      key: GlobalKey(),
-                      onChange: (item, isSelected) {
-                        if (isSelected)
-                          selectedTeeth.add(int.parse(item));
-                        else
-                          selectedTeeth.remove(int.parse(item));
-                        if (widget.onChange != null) widget.onChange!(selectedTeeth);
-                      },
-                      labels: [
-                        CIA_MultiSelectChipWidgeModel(label: "21", isSelected: widget.selectedTeeth!.contains(21)),
-                        CIA_MultiSelectChipWidgeModel(label: "22", isSelected: widget.selectedTeeth!.contains(22)),
-                        CIA_MultiSelectChipWidgeModel(label: "23", isSelected: widget.selectedTeeth!.contains(23)),
-                        CIA_MultiSelectChipWidgeModel(label: "24", isSelected: widget.selectedTeeth!.contains(24)),
-                        CIA_MultiSelectChipWidgeModel(label: "25", isSelected: widget.selectedTeeth!.contains(25)),
-                        CIA_MultiSelectChipWidgeModel(label: "26", isSelected: widget.selectedTeeth!.contains(26)),
-                        CIA_MultiSelectChipWidgeModel(label: "27", isSelected: widget.selectedTeeth!.contains(27)),
-                        CIA_MultiSelectChipWidgeModel(label: "28", isSelected: widget.selectedTeeth!.contains(28))
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-              SizedBox(height: 5),
-              Row(
-                children: [
-                  Expanded(
-                    child: CIA_MultiSelectChipWidget(
-                      key: GlobalKey(),
-                      onChange: (item, isSelected) {
-                        if (isSelected)
-                          selectedTeeth.add(int.parse(item));
-                        else
-                          selectedTeeth.remove(int.parse(item));
-                        if (widget.onChange != null) widget.onChange!(selectedTeeth);
-                      },
-                      labels: [
-                        CIA_MultiSelectChipWidgeModel(label: "48", isSelected: widget.selectedTeeth!.contains(48)),
-                        CIA_MultiSelectChipWidgeModel(label: "47", isSelected: widget.selectedTeeth!.contains(47)),
-                        CIA_MultiSelectChipWidgeModel(label: "46", isSelected: widget.selectedTeeth!.contains(46)),
-                        CIA_MultiSelectChipWidgeModel(label: "45", isSelected: widget.selectedTeeth!.contains(45)),
-                        CIA_MultiSelectChipWidgeModel(label: "44", isSelected: widget.selectedTeeth!.contains(44)),
-                        CIA_MultiSelectChipWidgeModel(label: "43", isSelected: widget.selectedTeeth!.contains(43)),
-                        CIA_MultiSelectChipWidgeModel(label: "42", isSelected: widget.selectedTeeth!.contains(42)),
-                        CIA_MultiSelectChipWidgeModel(label: "41", isSelected: widget.selectedTeeth!.contains(41)),
-                      ],
-                    ),
-                  ),
-                  Expanded(
-                    child: CIA_MultiSelectChipWidget(
-                      key: GlobalKey(),
-                      onChange: (item, isSelected) {
-                        if (isSelected)
-                          selectedTeeth.add(int.parse(item));
-                        else
-                          selectedTeeth.remove(int.parse(item));
-                        if (widget.onChange != null) widget.onChange!(selectedTeeth);
-                      },
-                      labels: [
-                        CIA_MultiSelectChipWidgeModel(label: "31", isSelected: widget.selectedTeeth!.contains(31)),
-                        CIA_MultiSelectChipWidgeModel(label: "32", isSelected: widget.selectedTeeth!.contains(32)),
-                        CIA_MultiSelectChipWidgeModel(label: "33", isSelected: widget.selectedTeeth!.contains(33)),
-                        CIA_MultiSelectChipWidgeModel(label: "34", isSelected: widget.selectedTeeth!.contains(34)),
-                        CIA_MultiSelectChipWidgeModel(label: "35", isSelected: widget.selectedTeeth!.contains(35)),
-                        CIA_MultiSelectChipWidgeModel(label: "36", isSelected: widget.selectedTeeth!.contains(36)),
-                        CIA_MultiSelectChipWidgeModel(label: "37", isSelected: widget.selectedTeeth!.contains(37)),
-                        CIA_MultiSelectChipWidgeModel(label: "38", isSelected: widget.selectedTeeth!.contains(38)),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-            ],
-          ),
-        ),
-        SizedBox(width: 10),
-        (selectedTeeth ?? []).toSet().length == 32
-            ? CIA_PrimaryButton(
-                isLong: true,
-                label: "Unselect All",
-                onTab: () {
-                  widget.selectedTeeth = [];
-                  setState(() => selectedTeeth = []);
-                })
-            : CIA_SecondaryButton(
-                label: "Select All",
-                onTab: () {
-                  selectedTeeth = [
-                    11,
-                    12,
-                    13,
-                    14,
-                    15,
-                    16,
-                    17,
-                    18,
-                    21,
-                    22,
-                    23,
-                    24,
-                    25,
-                    26,
-                    27,
-                    28,
-                    31,
-                    32,
-                    33,
-                    34,
-                    35,
-                    36,
-                    37,
-                    38,
-                    41,
-                    42,
-                    43,
-                    44,
-                    45,
-                    46,
-                    47,
-                    48,
-                  ];
-                  widget.selectedTeeth = selectedTeeth;
+        Row(
+          children: [
+            Expanded(
+              child: CIA_MultiSelectChipWidget(
+                key: GlobalKey(),
+                onChange: (item, isSelected) {
+                  if (isSelected)
+                    selectedTeeth.add(int.parse(item));
+                  else
+                    selectedTeeth.remove(int.parse(item));
                   if (widget.onChange != null) widget.onChange!(selectedTeeth);
-                  setState(() => null);
-                }),
-        SizedBox(width: 10),
+                },
+                labels: [
+                  CIA_MultiSelectChipWidgeModel(label: "18", isSelected: widget.selectedTeeth!.contains(18)),
+                  CIA_MultiSelectChipWidgeModel(label: "17", isSelected: widget.selectedTeeth!.contains(17)),
+                  CIA_MultiSelectChipWidgeModel(label: "16", isSelected: widget.selectedTeeth!.contains(16)),
+                  CIA_MultiSelectChipWidgeModel(label: "15", isSelected: widget.selectedTeeth!.contains(15)),
+                  CIA_MultiSelectChipWidgeModel(label: "14", isSelected: widget.selectedTeeth!.contains(14)),
+                  CIA_MultiSelectChipWidgeModel(label: "13", isSelected: widget.selectedTeeth!.contains(13)),
+                  CIA_MultiSelectChipWidgeModel(label: "12", isSelected: widget.selectedTeeth!.contains(12)),
+                  CIA_MultiSelectChipWidgeModel(label: "11", isSelected: widget.selectedTeeth!.contains(11)),
+                ],
+              ),
+            ),
+            Expanded(
+              child: CIA_MultiSelectChipWidget(
+                key: GlobalKey(),
+                onChange: (item, isSelected) {
+                  if (isSelected)
+                    selectedTeeth.add(int.parse(item));
+                  else
+                    selectedTeeth.remove(int.parse(item));
+                  if (widget.onChange != null) widget.onChange!(selectedTeeth);
+                },
+                labels: [
+                  CIA_MultiSelectChipWidgeModel(label: "21", isSelected: widget.selectedTeeth!.contains(21)),
+                  CIA_MultiSelectChipWidgeModel(label: "22", isSelected: widget.selectedTeeth!.contains(22)),
+                  CIA_MultiSelectChipWidgeModel(label: "23", isSelected: widget.selectedTeeth!.contains(23)),
+                  CIA_MultiSelectChipWidgeModel(label: "24", isSelected: widget.selectedTeeth!.contains(24)),
+                  CIA_MultiSelectChipWidgeModel(label: "25", isSelected: widget.selectedTeeth!.contains(25)),
+                  CIA_MultiSelectChipWidgeModel(label: "26", isSelected: widget.selectedTeeth!.contains(26)),
+                  CIA_MultiSelectChipWidgeModel(label: "27", isSelected: widget.selectedTeeth!.contains(27)),
+                  CIA_MultiSelectChipWidgeModel(label: "28", isSelected: widget.selectedTeeth!.contains(28))
+                ],
+              ),
+            ),
+            SizedBox(width: 10),
+            () {
+              bool contains = true;
+              for (var element in [
+                11,
+                12,
+                13,
+                14,
+                15,
+                16,
+                17,
+                18,
+                21,
+                22,
+                23,
+                24,
+                25,
+                26,
+                27,
+                28,
+              ]) {
+                if (!selectedTeeth.contains(element)) {
+                  contains = false;
+                  break;
+                }
+              }
+              return contains;
+            }()
+                ? CIA_PrimaryButton(
+                    label: "Unselect Upper",
+                    isLong: true,
+                    onTab: () {
+                      selectedTeeth.removeWhere((element) => [
+                            11,
+                            12,
+                            13,
+                            14,
+                            15,
+                            16,
+                            17,
+                            18,
+                            21,
+                            22,
+                            23,
+                            24,
+                            25,
+                            26,
+                            27,
+                            28,
+                          ].contains(element));
+                      widget.selectedTeeth = selectedTeeth;
+                      setState(() => null);
+                    })
+                : CIA_SecondaryButton(
+                    label: "Select Upper",
+                    onTab: () {
+                      selectedTeeth.addAll([
+                        11,
+                        12,
+                        13,
+                        14,
+                        15,
+                        16,
+                        17,
+                        18,
+                        21,
+                        22,
+                        23,
+                        24,
+                        25,
+                        26,
+                        27,
+                        28,
+                      ]);
+                      selectedTeeth = selectedTeeth.toSet().toList();
+                      widget.selectedTeeth = selectedTeeth;
+                      if (widget.onChange != null) widget.onChange!(selectedTeeth);
+                      setState(() => null);
+                    })
+          ],
+        ),
+        SizedBox(height: 5),
+        Row(
+          children: [
+            Expanded(
+              child: CIA_MultiSelectChipWidget(
+                key: GlobalKey(),
+                onChange: (item, isSelected) {
+                  if (isSelected)
+                    selectedTeeth.add(int.parse(item));
+                  else
+                    selectedTeeth.remove(int.parse(item));
+                  if (widget.onChange != null) widget.onChange!(selectedTeeth);
+                },
+                labels: [
+                  CIA_MultiSelectChipWidgeModel(label: "48", isSelected: widget.selectedTeeth!.contains(48)),
+                  CIA_MultiSelectChipWidgeModel(label: "47", isSelected: widget.selectedTeeth!.contains(47)),
+                  CIA_MultiSelectChipWidgeModel(label: "46", isSelected: widget.selectedTeeth!.contains(46)),
+                  CIA_MultiSelectChipWidgeModel(label: "45", isSelected: widget.selectedTeeth!.contains(45)),
+                  CIA_MultiSelectChipWidgeModel(label: "44", isSelected: widget.selectedTeeth!.contains(44)),
+                  CIA_MultiSelectChipWidgeModel(label: "43", isSelected: widget.selectedTeeth!.contains(43)),
+                  CIA_MultiSelectChipWidgeModel(label: "42", isSelected: widget.selectedTeeth!.contains(42)),
+                  CIA_MultiSelectChipWidgeModel(label: "41", isSelected: widget.selectedTeeth!.contains(41)),
+                ],
+              ),
+            ),
+            Expanded(
+              child: CIA_MultiSelectChipWidget(
+                key: GlobalKey(),
+                onChange: (item, isSelected) {
+                  if (isSelected)
+                    selectedTeeth.add(int.parse(item));
+                  else
+                    selectedTeeth.remove(int.parse(item));
+                  if (widget.onChange != null) widget.onChange!(selectedTeeth);
+                },
+                labels: [
+                  CIA_MultiSelectChipWidgeModel(label: "31", isSelected: widget.selectedTeeth!.contains(31)),
+                  CIA_MultiSelectChipWidgeModel(label: "32", isSelected: widget.selectedTeeth!.contains(32)),
+                  CIA_MultiSelectChipWidgeModel(label: "33", isSelected: widget.selectedTeeth!.contains(33)),
+                  CIA_MultiSelectChipWidgeModel(label: "34", isSelected: widget.selectedTeeth!.contains(34)),
+                  CIA_MultiSelectChipWidgeModel(label: "35", isSelected: widget.selectedTeeth!.contains(35)),
+                  CIA_MultiSelectChipWidgeModel(label: "36", isSelected: widget.selectedTeeth!.contains(36)),
+                  CIA_MultiSelectChipWidgeModel(label: "37", isSelected: widget.selectedTeeth!.contains(37)),
+                  CIA_MultiSelectChipWidgeModel(label: "38", isSelected: widget.selectedTeeth!.contains(38)),
+                ],
+              ),
+            ),
+            SizedBox(width: 10),
+            () {
+              bool contains = true;
+              for (var element in [
+                31,
+                32,
+                33,
+                34,
+                35,
+                36,
+                37,
+                38,
+                41,
+                42,
+                43,
+                44,
+                45,
+                46,
+                47,
+                48,
+              ]) {
+                if (!selectedTeeth.contains(element)) {
+                  contains = false;
+                  break;
+                }
+              }
+              return contains;
+            }()
+                ? CIA_PrimaryButton(
+                    label: "Unselect Lower",
+                    isLong: true,
+                    onTab: () {
+                      selectedTeeth.removeWhere((element) => [
+                            31,
+                            32,
+                            33,
+                            34,
+                            35,
+                            36,
+                            37,
+                            38,
+                            41,
+                            42,
+                            43,
+                            44,
+                            45,
+                            46,
+                            47,
+                            48,
+                          ].contains(element));
+                      widget.selectedTeeth = selectedTeeth;
+                      setState(() => null);
+                    })
+                : CIA_SecondaryButton(
+                    label: "Select Lower",
+                    onTab: () {
+                      selectedTeeth.addAll([
+                        31,
+                        32,
+                        33,
+                        34,
+                        35,
+                        36,
+                        37,
+                        38,
+                        41,
+                        42,
+                        43,
+                        44,
+                        45,
+                        46,
+                        47,
+                        48,
+                      ]);
+                      selectedTeeth = selectedTeeth.toSet().toList();
+                      widget.selectedTeeth = selectedTeeth;
+                      if (widget.onChange != null) widget.onChange!(selectedTeeth);
+                      setState(() => null);
+                    })
+          ],
+        ),
       ],
     );
   }
