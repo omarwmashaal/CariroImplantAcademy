f522d3f074e78a4b7f6dab0a1722bf2aea078ad2
Omar Mashaal <omarwmashaal@gmail.com>
2024-05-03 23:18:19 +0300

    assert not null treatment it when save treatment plan

--- cariro_implant_academy/lib/features/patientsMedical/treatmentFeature/domain/usecase/saveTreatmentDetailsUseCase.dart
+++ cariro_implant_academy/lib/features/patientsMedical/treatmentFeature/domain/usecase/saveTreatmentDetailsUseCase.dart
@@ -3,6 +3,7 @@ import 'package:cariro_implant_academy/core/useCases/useCases.dart';
 import 'package:cariro_implant_academy/features/patientsMedical/treatmentFeature/domain/entities/treatmenDetailsEntity.dart';
 import 'package:cariro_implant_academy/features/patientsMedical/treatmentFeature/domain/repo/treatmentPlanRepo.dart';
 import 'package:dartz/dartz.dart';
+import 'package:get/get.dart';
 
 class SaveTreatmentDetailsUseCase extends UseCases<NoParams, SaveTreatmentDetailsParams> {
   final TreatmentPlanRepo treatmentPlanRepo;
@@ -11,7 +12,9 @@ class SaveTreatmentDetailsUseCase extends UseCases<NoParams, SaveTreatmentDetail
 
   @override
   Future<Either<Failure, NoParams>> call(SaveTreatmentDetailsParams data) async {
-    //data.data.removeWhere((element) => element.isNull());
+    if (data.data.firstWhereOrNull((element) => element.treatmentItemId == null) != null) {
+      return Left(BadRequestFailure(failureMessage: "Error In Treatment Item"));
+    }
     return await treatmentPlanRepo.saveTreatmentDetailsData(data.id, data.data).then(
           (value) => value.fold(
             (l) => Left(l..message = "Save Treatment Plan: ${l.message ?? ""}"),
